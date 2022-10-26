ffa9aae54a7ca4c8d95b0749a31b7162dc92672a
unclesmoove <alex.f.ushe@gmail.com>
2022-10-26 12:09:14 +0200

    Messaging app initial commit

---
+++ src/app/posts/post.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Subject}from 'rxjs';
+import { Post } from "./post.model";
+
+@Injectable({providedIn: 'root'})
+export class PostsService{
+  private posts: Post[]=[]; //storing  a list of posts
+  private postsUpdated = new Subject<Post[]>();
+
+  getPosts(){
+    return [...this.posts]; //making a new copy of the o.g. array.
+  }
+  getPostUpdateListener(){
+    return this.postsUpdated.asObservable();
+  }
+  addPost(title:string,content:string){
+    const post: Post = {title:title,content:content};
+    this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
+  }
+}
